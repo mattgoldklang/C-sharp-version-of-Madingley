b427f21beaf3eaa61eeb4ce2b0857c0a60f90281
mattgoldklang <mattgoldklang@gmail.com>
2018-11-23 14:05:12 +0100

    Update AutotrophProcessor.cs

--- Madingley/Ecology/Ecological processes stocks/AutotrophProcessor.cs
+++ Madingley/Ecology/Ecological processes stocks/AutotrophProcessor.cs
@@ -63,21 +63,55 @@ namespace Madingley
         /// <param name="outputDetail">The level of output detail to use for the outputs</param>
         /// <param name="specificLocations">Whether the model is being run for specific locations</param>
         /// <param name="currentMonth">The current month in the model run</param>
+        public double() sizedep(a,b,V)
+        {
+            double p = a*Math.Pow(V,b);
+            return p   
+        }
         public void ConvertNPPToAutotroph(FunctionalGroupDefinitions cohortDefinitions, FunctionalGroupDefinitions stockDefinitions,
             SortedList<string,double[]> cellEnvironment, GridCellStockHandler gridCellStockHandler, int[] 
             actingStock, string terrestrialNPPUnits, string oceanicNPPUnits, uint currentTimestep, string GlobalModelTimeStepUnit,
             ProcessTracker trackProcesses, FunctionalGroupTracker functionalTracker, GlobalProcessTracker globalTracker, string outputDetail, bool specificLocations,uint currentMonth)
         {
             double NPP = new double();
+            double RGR = new double();
+            double avphytosize = new double();
+            double no3 = new double();
+            double temperature = new double();
 
             // Check that this is an ocean cell
             if(cellEnvironment["Realm"][0] == 2.0)
             {
+                no3 = cellEnvironment["no3"][currentMonth];
+                temperature = cellEnvironmnet["sst"][currentMonth];
+                double mld = cellEnvironment["mld"][currentMonth];
+                double[] phyto = new double[3];
+                double[] nsfPhyto = new double[3];
+                double[] phytoSize = new double[3] { Math.Log10(1E-9), Math.Log10(1E-7), Math.Log10(1E-5) };
+                double[] regCoefs = new double[2];
+
+                phyto[0] = 1.145 - 0.021 * no3 - 6.936E-6 * temperature;
+                phyto[1] = 1.146 + 0.013 * no3 - 0.064 * temperature;
+                phyto[2] = 0.804 - 0.002 * no3 - 0.077 * temperature;
+                nsfPhyto[0] = Math.Log10(Math.Exp(phyto[0]) / (Math.Exp(phyto[0]) + Math.Exp(phyto[1]) + Math.Exp(phyto[2])));
+                nsfPhyto[1] = Math.Log10(Math.Exp(phyto[1]) / (Math.Exp(phyto[0]) + Math.Exp(phyto[1]) + Math.Exp(phyto[2])));
+                nsfPhyto[2] = Math.Log10(Math.Exp(phyto[2]) / (Math.Exp(phyto[0]) + Math.Exp(phyto[1]) + Math.Exp(phyto[2])));
+                
+                i = 0;
+                avphytosize = 0.0;
+                while(i<3,i++)
+                {
+                    avphytosize += nsfPhyto[i]*phytoSize[i];
+                }
+                double sr = sizedep(.28,.39,avphytosize);
+                double kn = sizedep(.17,-.27,avphytosize);
+                double grmax = sizedep(5.37,-.25,avphytosize);
+                double mort = 0.02;
                 switch (gridCellStockHandler[actingStock].StockName)
                 {
                     case "oceannpp":
                         // Get NPP from cell environment
-                        NPP = cellEnvironment["NPP"][currentMonth];
+                        NPP = cellEnvironment["gC"][currentMonth];
 
                         // If NPP is a missing value then set to zero
                         if (NPP == cellEnvironment["Missing Value"][0]) NPP = 0.0;
@@ -89,9 +123,9 @@ namespace Madingley
                         Console.WriteLine("Can't find phytoplankton name in stocks");
                         break;
                 }
-
+                NPP = gC*((grmax*Math.Pow(1.066,(20-sst)*(no3/(no3+kn)) - (mort + sr))*mld*1E-3;
                 // Check that the units of oceanic NPP are gC per m2 per day
-                Debug.Assert(oceanicNPPUnits == "gC/m2/day", "Oceanic NPP data are not in the correct units for this formulation of the model");
+                //Debug.Assert(oceanicNPPUnits == "gC/m2/day", "Oceanic NPP data are not in the correct units for this formulation of the model");
 
                 // Convert to g/cell/month
                 NPP *= _MsqToKmSqConversion;
